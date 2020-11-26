bc66e60ab63cd196188da9cef70b59e4fd62d9bd
Artem Astrakhantsev <tembaster@gmail.com>
2020-11-26 21:06:54 +0300

    HW06 implement 1.3 in process..

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -1,29 +1,6 @@
 package ru.javawebinar.topjava.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import ru.javawebinar.topjava.service.MealService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Objects;
-
-@Controller
 public class JspMealController {
 
 
-    @Autowired
-    private MealService mealService;
-
-    @GetMapping("/meals")
-    public String getMeals(Model model, int id) {
-        model.addAttribute("meals", mealService.getAll(id));
-        return "meals";
-    }
-
-    private int getId(HttpServletRequest request) {
-        String paramId = Objects.requireNonNull(request.getParameter("id"));
-        return Integer.parseInt(paramId);
-    }
 }
